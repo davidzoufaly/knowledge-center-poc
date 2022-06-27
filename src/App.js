fadc8959098bf3c55fb3a8956dc53ebcfbf1f21d
davidzoufaly <davidzoufaly@gmail.com>
2022-06-27 15:38:27 +0200

    POC knowledge center

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import test from "./test.md";
+// styling can be done via pseudoselctors
+import "./App.css";
 
 function App() {
+  const [mdFile, setMdFile] = useState();
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(() => {
+    fetch(test)
+      .then((response) => response.text())
+      .then((text) => {
+        setMdFile(text);
+      });
+  }, []);
+
+  useEffect(() => {
+    const menuItemsArray = Array.from(document.querySelectorAll("h2"));
+    setMenuItems(menuItemsArray);
+    // assign id as anchor for menu link
+    menuItemsArray.forEach((item, i) => (item.id = `h2-${i}`));
+  }, [mdFile]);
+
+  // Id and link should be contained from h2 innerText -> we can use our stringMethods for transforming to correct notation
+  const menu = (
+    <ul>
+      {menuItems.map((item, index) => (
+        <li key={item.innerText}>
+          <a href={`#h2-${index}`}>{item.innerText}</a>
+        </li>
+      ))}
+    </ul>
+  );
+
+  useEffect(() => {
+    console.log("mdFile: ", mdFile);
+  }, [mdFile]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <p>menu</p>
+        {menu}
+      </nav>
+      <main>
+        <p>content</p>
+        <ReactMarkdown children={mdFile} />
+      </main>
+    </>
   );
 }
 
---
+++ src/test.md
@@ -0,0 +1,22 @@
+# hello
+## testing markdown
+### heading 3
+* Bold text *
+- list item
+- list item
+
+## another section
+### heading 3
+text
+
+## another section 2
+### heading 3
+text2
+
+## another section 3
+### heading 3
+text3
+
+## another section 4
+### heading 3
+text4
